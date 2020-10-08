25ccc80c9bdaf29e975a48e9a68f722e2269cbf3
b-lack <gerrit.balindt@b-lack.io>
2020-10-08 13:55:59 +0200

    add changes

---
+++ build/precache-manifest.1b20b13efc2338f30103458353a97ded.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "8e0c333fb01603bfe609d7fe83ff2f41",
+    "url": "/eldat-form/build/index.html"
+  },
+  {
+    "revision": "c2b2a2ff7e719789cce1",
+    "url": "/eldat-form/build/static/css/main.65dee7f8.chunk.css"
+  },
+  {
+    "revision": "c5aafa10a0a6b6e54128",
+    "url": "/eldat-form/build/static/js/2.f016ac87.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/eldat-form/build/static/js/2.f016ac87.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "c2b2a2ff7e719789cce1",
+    "url": "/eldat-form/build/static/js/main.e2387522.chunk.js"
+  },
+  {
+    "revision": "a852442f1839ace2df59",
+    "url": "/eldat-form/build/static/js/runtime-main.406fd276.js"
+  }
+]);
